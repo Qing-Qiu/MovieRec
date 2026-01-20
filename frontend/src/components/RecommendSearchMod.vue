68b25dcd61cdec1c316a51bd5a4ebcee575dd62f
Qing-Qiu <1223613942@qq.com>
2026-01-20 23:52:15 +0800

    修复首页搜索按钮展示问题

--- frontend/src/components/RecommendSearchMod.vue
+++ frontend/src/components/RecommendSearchMod.vue
@@ -6,6 +6,7 @@
             v-model:value="formState.search"
             placeholder="Search for movies, actors, directors..."
             size="large"
+            enter-button
             @search="submitForm"
             class="custom-search"
         />
